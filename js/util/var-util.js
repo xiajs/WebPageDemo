8c3f87d37aaf0fc20713dbdd4ac18489f2e38d87
xiajs <xia830727@hotmail.com>
2018-11-26 14:39:04 +0800

    20181126
    增加var-util.js文件

---
+++ js/util/var-util.js
@@ -0,0 +1,44 @@
+/**
+ * Created by xiajs on 2018/11/26.
+ *
+ *  测试代码：
+ *  alert("为空判断 1 = "+varUtil.isEmptyStr("1"));
+ *  alert("为空判断 空引号 = "+varUtil.isEmptyStr(""));
+ *  alert("为空判断 空格 = "+varUtil.isEmptyStr(" "));
+ *  alert("为空判断 null = "+varUtil.isEmptyStr(null));
+ *  alert("为空判断 无参 = "+varUtil.isEmptyStr());
+ *
+ */
+
+varUtil = (function(){
+    var innerUtil = {
+
+        /**
+         * @param param
+         */
+       isEmptyStr:function(param){
+            if (param == null || param == undefined || param ==  ''){
+                return true;
+            }
+
+            if (param.length == 0 || param.trim().length == 0){
+                return true;
+            }
+
+            return false;
+        },
+        isNullStr:function(param){
+
+            if (param == null || param == undefined || param ==  ''){
+                return true;
+            }
+
+            if (param.length == 0 ){
+                return true;
+            }
+
+            return false;
+        }
+    };
+    return innerUtil;
+})();
